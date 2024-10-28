314001a76a19ff2badcfd61be158164891459ac6
mr coder <emamskingdom@gmail.com>
2024-10-29 03:05:02 +0600

    added input onclick

--- script/ractangle-calculate.js
+++ script/ractangle-calculate.js
@@ -1,3 +1,25 @@
 function calculateRectangleArea(){
-    console.log('connected rectangle')
+
+    // get rectangle length
+    const rectangleLengthInput = document.getElementById('rectangle-length')
+    const lengthValue = rectangleLengthInput.value;
+    const length = parseFloat(lengthValue)
+    console.log(length)
+
+
+    // get ractangle width
+
+    const rectangleWidthInput = document.getElementById('rectangle-width')
+    const WidthValue = rectangleWidthInput.value;
+    const width = parseFloat(WidthValue)
+    console.log(width)
+
+    // calculate ractangle area
+    const area = length * width;
+
+    // set the area number of display
+    const rectangleAreaSpan = document.getElementById('rectangle-l')
+    rectangleAreaSpan.innerText = length;
+    const rectangleAreaSpan2 = document.getElementById('rectangle-w')
+    rectangleAreaSpan2.innerText = width;
 }
