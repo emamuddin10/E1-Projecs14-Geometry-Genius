da04ed5107ec7f17da6543b6c15d80aa44531ee9
mr coder <emamskingdom@gmail.com>
2024-10-29 15:09:19 +0600

    set innerText perallelogram

---
+++ script/perallelogram-calculate.js
@@ -0,0 +1,36 @@
+ 
+//  function parallelogramCalculateArea(){
+//     const base = getParallelogramBase()
+//     console.log(base)
+//  }
+
+//  function getParallelogramBase(){
+//     const parallelogramBaseInput = document.getElementById('parallelogram-base');
+//     const parallelogramBaseText = parallelogramBaseInput.value;
+//     const base = parseFloat(parallelogramBaseText)
+//     return base
+//  }
+
+function calculateParallelogramArea(){
+   
+  const base =  getInputValue('parallelogram-base')
+  const height = getInputValue('parallelogram-height')
+
+  const area = base*height
+  console.log(area)
+
+  setParallelogramInnerText('parallelogram-output',area)
+
+}
+function getInputValue(getInputFieldId){
+    const inputField = document.getElementById(getInputFieldId);
+    const inputFieldText = inputField.value;
+    const inputValue = parseFloat(inputFieldText)
+    return inputValue ;
+
+}
+
+function setParallelogramInnerText(elementId, area){
+    const element = document.getElementById(elementId)
+    element.innerText = area 
+}
